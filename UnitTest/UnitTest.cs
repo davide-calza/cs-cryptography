dc79719319cb6c26c4832c69bf008bc5c1121057
daddolo <davide20799@gmail.com>
2018-01-16 17:46:31 +0100

    Unit Test: add Generate txt Files List method

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
 
 namespace UnitTest
 {
@@ -13,20 +14,27 @@ namespace UnitTest
         [TestMethod]
         public void TestMd5TxtList()
         {
-            var dir = ConfigurationManager.AppSettings.Get("test_dir");
-            var text_1 = ConfigurationManager.AppSettings.Get("txt_file_text_1");
             var md5_1 = ConfigurationManager.AppSettings.Get("txt_file_md5_1").ToUpper();
-            var text_2 = ConfigurationManager.AppSettings.Get("txt_file_text_2");
             var md5_2 = ConfigurationManager.AppSettings.Get("txt_file_md5_2").ToUpper();
-            var text_3 = ConfigurationManager.AppSettings.Get("txt_file_text_3");
             var md5_3 = ConfigurationManager.AppSettings.Get("txt_file_md5_3").ToUpper();
-            var list = new List<string> {dir + "/test1.txt", dir + "/test2.txt", dir + "/test3.txt"};
+            var list = GenerateTxtFilesList();
+            var enumerable = list as string[] ?? list.ToArray();
             var sol = new List<string>
             {
-                list.ElementAt(0) + " = " + md5_1,
-                list.ElementAt(1) + " = " + md5_2,
-                list.ElementAt(2) + " = " + md5_3
+                enumerable.ElementAt(0) + " = " + md5_1,
+                enumerable.ElementAt(1) + " = " + md5_2,
+                enumerable.ElementAt(2) + " = " + md5_3
             };
+            CollectionAssert.AreEqual(ClassAcm.CalculateTxtMd5List(enumerable).ToList(), sol);
+        }
+
+        private static IEnumerable<string> GenerateTxtFilesList()
+        {
+            var dir = ConfigurationManager.AppSettings.Get("test_dir");
+            var text1 = ConfigurationManager.AppSettings.Get("txt_file_text_1");
+            var text2 = ConfigurationManager.AppSettings.Get("txt_file_text_2");
+            var text3 = ConfigurationManager.AppSettings.Get("txt_file_text_3");
+            var list = new List<string> {dir + "/test1.txt", dir + "/test2.txt", dir + "/test3.txt"};
             if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
             foreach (var l in list)
             {
@@ -36,13 +44,13 @@ namespace UnitTest
                     switch (list.IndexOf(l))
                     {
                         case 0:
-                            sw.WriteLine(text_1);
+                            sw.WriteLine(text1);
                             break;
                         case 1:
-                            sw.WriteLine(text_2);
+                            sw.WriteLine(text2);
                             break;
                         case 2:
-                            sw.WriteLine(text_3);
+                            sw.WriteLine(text3);
                             break;
                         default: break;
                     }
@@ -51,7 +59,7 @@ namespace UnitTest
                 }
             }
 
-            CollectionAssert.AreEqual(ClassAcm.CalculateTxtMd5List(list).ToList(), sol);
+            return list;
         }
     }
 }
