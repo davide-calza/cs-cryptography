7f7b0d828c2fb10b0916e8c507f1c351c3e30ccb
daddolo <davide20799@gmail.com>
2018-01-25 19:38:36 +0100

    File validity check

--- ConsoleRSA/Program.cs
+++ ConsoleRSA/Program.cs
@@ -31,8 +31,8 @@ namespace ConsoleRSA
 
                 switch(com)
                 {
-                    case "ENC": Console.WriteLine(Encode(inp, outp, keyFile)); break;
-                    case "DEC": Console.WriteLine(Decode(inp, outp, keyFile)); break;
+                    case "ENC": Console.WriteLine(Encrypt(inp, outp, keyFile)); break;
+                    case "DEC": Console.WriteLine(Decrypt(inp, outp, keyFile)); break;
                     default: throw new Exception("Unknown command"); 
                 }
             }
@@ -42,16 +42,59 @@ namespace ConsoleRSA
                 Help();
             }		
         }
-        private static string Encode(string input, string output, string keyFile)
+
+        /// <summary>
+        /// Encrypts a file with RSA algorithm
+        /// </summary>
+        /// <param name="input">input txt file</param>
+        /// <param name="output">output rsa file</param>
+        /// <param name="keyFile">xml key file</param>
+        /// <returns></returns>
+        private static string Encrypt(string input, string output, string keyFile)
         {
-            // verifica parametri e cifrare
-            return "Encode ... da fare ...";
+            try
+            {
+                if (!File.Exists(input))
+                    throw new Exception(input + " not found");
+                if (!File.Exists(keyFile))
+                    throw new Exception(keyFile + " not found");
+                if (!Directory.Exists(Path.GetDirectoryName(output)))
+                    throw new Exception("Output directory not found");
+
+                return "File successfully encrypted";
+            }
+            catch (Exception e)
+            {
+                return "Exception on encryption: " + e.Message;
+            }
         }
-        private static string Decode(string input, string output, string keyFile)
+
+        /// <summary>
+        /// Decode a RSA encrypted file
+        /// </summary>
+        /// <param name="input">input rsa file</param>
+        /// <param name="output">output txt file</param>
+        /// <param name="keyFile">xml key file</param>
+        /// <returns></returns>
+        private static string Decrypt(string input, string output, string keyFile)
         {
-            // verifica parametri e decifrare
-            return "Decode ... da fare ...";
+            try
+            {
+                if (!File.Exists(input))
+                    throw new Exception(input + " not found");
+                if (!File.Exists(keyFile))
+                    throw new Exception(keyFile + " not found");
+                if (!Directory.Exists(Path.GetDirectoryName(output)))
+                    throw new Exception("Output directory not found");
+
+                return "File successfully encrypted";
+            }
+            catch (Exception e)
+            {
+                return "Exception on decryption: " + e.Message;
+            }
         }
+
         /// <summary>
         /// Print help summaryS
         /// </summary>
